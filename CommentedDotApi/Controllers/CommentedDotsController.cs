995c40a76828a86ae478e9e9d0c1162456d54280
Konstantin Gromov <Gromov.Konst95@gmail.com>
2022-08-02 19:29:56 +0300

    Add Controller
    Added template controller for the future

---
+++ CommentedDotApi/Controllers/CommentedDotsController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommentedDotApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommentedDotsController : ControllerBase
+    {
+        List<string> values = new List<string> { "value 0", "value 1", "value 2" };
+
+        [HttpGet]
+        [Route("GetAll")]
+        public IEnumerable<string> GetAll()
+        {
+            return values;
+        }
+
+        [HttpGet]
+        [Route("GetById")]
+        public string GetById(int id)
+        {
+            return values[id];
+        }
+
+        [HttpDelete]
+        [Route("Delete")]
+        public void Delete(int id)
+        {
+            values.RemoveAt(id);
+        }
+    }
+}
