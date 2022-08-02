cb006efb460b63c0a797984b3f99a91da94e4447
Konstantin Gromov <Gromov.Konst95@gmail.com>
2022-08-02 23:46:23 +0300

    Add DbContext
    Adds EntityFrameworkCore implementation.

---
+++ CommentedDotApi.Library/DataAccess/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using CommentedDotApi.Library.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommentedDotApi.Library.DataAccess
+{
+    public class AppDbContext : DbContext
+    {
+
+        List<DotModel> Dots { get; set; } = null!;
+
+        public AppDbContext( DbContextOptions<AppDbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<CommentModel>();
+        }
+    }
+}
